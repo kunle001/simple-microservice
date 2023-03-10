bbc98a66eccdaded53f8ffc624821d283bb1948a
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-03-10 23:24:44 +0100

    services features updated

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const cors= require('cors')
 
+const events= []
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
+
 
 app.post("/events", (req, res) => {
   const event = req.body;
-
+  events.push(req.body);
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
   });
@@ -22,6 +26,10 @@ app.post("/events", (req, res) => {
   });
   res.send({ status: "OK" });
 });
+
+app.get('/events', (req,res)=>{
+  res.send(events)
+})
 
 app.listen(4005, () => {
   console.log("Listening on 4005");
