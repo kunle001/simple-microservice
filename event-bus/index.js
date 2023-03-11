fd2e0602e444dc05a4fe77b2e9294cece063bbc4
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-03-11 23:12:28 +0100

    making moderations based on deployments and services

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,16 +12,16 @@ app.use(cors())
 app.post("/events", (req, res) => {
   const event = req.body;
   events.push(req.body);
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post("http://post-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
+  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
